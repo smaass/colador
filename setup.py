431f7062399c378fc50300b7e4c36e8e88a429d6
Sergio Maass <sergio@botcenter.io>
2019-03-25 00:33:07 -0300

    now?

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['colador'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
